f1c41d4f8c31c5371cd86fb22231c2cc769e92e1
Ktmich2095 <230881@utxicotepec.edu.mx>
2024-09-19 16:44:54 -0600

    Practica 4
    Agregación de los tipos de datos String, Bit init y Symbol

---
+++ JS/ejercicio_03.js
@@ -0,0 +1,27 @@
+//tipos de datos
+//Strings o cadenas de texto
+// const alumno = "Michelle"
+// let producto = "Monitor 20 pulgadas"
+// const numero = "30"
+// const numeroDos = 30
+// console.log(typeof numero)
+// console.log(typeof numeroDos)
+//BigInt
+// const numeroGrande = BigInt(9876212345678876543234234554567896)
+// console.log(typeof numeroGrande)
+// const numero = 10
+// const numeroDos = 20 
+// console.log(numero + Number(numeroGrande))
+// const numero = "30"
+// const numeroDos = 30
+// console.log(typeof String(numeroDos))
+// console.log(typeof Number(numero))
+//Symbol
+// const primerSymbol = Symbol(30)
+// const segundoSymbol = Symbol(30)
+// console.log(primerSymbol === segundoSymbol)
+// console.log(primerSymbol.valueOf())
+// console.log(segundoSymbol.valueOf())
+//null
+// const descuento = null
+// console.log(typeof descuento)
